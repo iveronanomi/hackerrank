b5aeecbd75148b46385dc7560c7c94161e4de5d6
Eremin Ivan <coding.ebola@gmail.com>
2016-08-10 12:56:37 +0300

    utils for tests

---
+++ utils/utils.go
@@ -0,0 +1,40 @@
+package utils
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func CaptureStdout(f func()) string {
+	oldStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	f()
+
+	w.Close()
+	os.Stdout = oldStdout
+	var buf bytes.Buffer
+	io.Copy(&buf, r)
+	return buf.String()
+}
+
+func WriteInput(data string) *os.File {
+	in, err := ioutil.TempFile("", "")
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = io.WriteString(in, data)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = in.Seek(0, os.SEEK_SET)
+	if err != nil {
+		panic(err)
+	}
+	return in
+}
