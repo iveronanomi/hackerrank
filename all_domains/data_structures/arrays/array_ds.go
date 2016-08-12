5b824d685edc0277b36a80a254987eeab0ba2bac
Eremin Ivan <coding.ebola@gmail.com>
2016-08-12 10:16:35 +0300

    all_domains/data_structures/arrays real answer - not revers reading)

--- all_domains/data_structures/arrays/array_ds.go
+++ all_domains/data_structures/arrays/array_ds.go
@@ -13,8 +13,14 @@ func ArrayDS(getInput utils.GetInput) {
 	fmt.Fscanf(input, "%v", &length)
 	arr := make([]string, length)
 
-	for i := length - 1; i >= 0; i-- {
+	//for i := length - 1; i >= 0; i-- {
+	//	fmt.Fscanf(input, "%v", &arr[i])
+	//}
+	for i := int32(0); i < length; i++ {
 		fmt.Fscanf(input, "%v", &arr[i])
+	}
+	for i, j := 0, len(arr)-1; i < j; i, j = i+1, j-1 {
+		arr[i], arr[j] = arr[j], arr[i]
 	}
 	fmt.Print(strings.Join(arr, " "))
 }
